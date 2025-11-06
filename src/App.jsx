d7d244bd5f71a68e45b6ba9e94dcdfc1d8ab18bc
Soumya Nandi <soumyanandi2003@gmail.com>
2025-11-06 23:52:33 +0530

    adding some basic requirment and setting up the folders !

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
 export const Test =()=>{
   return (
     <>
-    <div className="first">
-      <h2 className="heading">
+    <div className="first  ">
+      <h2 className="heading border-[1px] mt-4 p-4 border-white rounded-md">
         Hello from soumya's broken laptop
       </h2>
     </div>
